ddc9e6b6a0edd80e3467f3a176862b19d7fd1e7f
Pieter Maene <pieter@maene.eu>
2015-03-01 16:44:40 +0100

    Created plist and class for constants

---
+++ Dyr/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  Dyr
+//
+//  Created by Pieter Maene on 15/11/14.
+//  Copyright (c) 2014 Student IT vzw. All rights reserved.
+//
+
+class Constants {
+    let apiBaseUrl: String = {
+        let constantsDictionary: NSDictionary = NSDictionary(contentsOfFile: "Constants.plist")!
+        return constantsDictionary.objectForKey("APIBaseURL") as String
+    }()
+    
+    let apiClientID: String = {
+        let constantsDictionary: NSDictionary = NSDictionary(contentsOfFile: "Constants.plist")!
+        return constantsDictionary.objectForKey("APIClientID") as String
+    }()
+}
